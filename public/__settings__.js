41b6f67d3691f75aa6715166c4d2910386bb2ff8
ff-seungju <hong01@fitfuns.com>
2022-08-09 08:29:52 +0900

    playcanvas 추가

---
+++ public/__settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1496168.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+SCRIPTS = [  ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/96881690/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/96881691/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/96881689/1/ammo.js', 'preload' : true},
+];
